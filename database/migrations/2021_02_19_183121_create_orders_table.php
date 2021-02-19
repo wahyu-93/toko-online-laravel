3ee9f5b1ac747db7510afeb5fe1bfef4df118428
wahyu yudi <wahyu.exam07@gmail.com>
2021-02-20 02:39:42 +0800

    make migration table video 1

---
+++ database/migrations/2021_02_19_183121_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->integer('user_id')->nullable();
+            $table->string('product_name');
+            $table->text('product_description');
+            $table->integer('product_price');
+            $table->string('user_name');
+            $table->string('user_phone');
+            $table->string('user_address');
+            $table->integer('qty');
+            $table->integer('subtotal');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
