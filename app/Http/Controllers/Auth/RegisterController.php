028971b8445a1ccc237be532011106ab9a017430
wahyu yudi <wahyu.exam07@gmail.com>
2021-02-21 09:34:14 +0800

    Hapus error i

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,8 +52,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'iaddress'  => ['required'],
-            'iphone'    => ['required', 'max:12']
+            'address'  => ['required'],
+            'phone'    => ['required', 'max:12']
         ]);
     }
 
